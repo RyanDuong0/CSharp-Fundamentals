e3d38cef7a9e66180278d1cab7d239999d33a726
RyanDuong0 <ryan.duong@aol.co.uk>
2026-03-02 14:22:53 +0000

    Added basics directory with initial Text.cs

---
+++ Basics/Text.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Fundamentals.Basics
+{
+    public class Text
+    {
+        //First section in the C# documentation: https://learn.microsoft.com/en-us/dotnet/csharp/tour-of-csharp/tutorials/
+        public static void main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
-﻿namespace C__Fundamentals
+﻿using C__Fundamentals.Basics;
+
+namespace C__Fundamentals
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //goal is to use a while loop and switch statement to "gameify" my notes.
+            //allow the user to select a topic and then display the notes for that topic
+
+            Text text = new Text();
         }
     }
 }
